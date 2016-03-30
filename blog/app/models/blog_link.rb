93a1dda652c67c1e5a437c0febcd7369c8239da2
Kat Landreth <katlandreth@gmail.com>
2016-03-30 10:48:18 -0500

    preview post in preview tab from index page

--- blog/app/models/blog_link.rb
+++ blog/app/models/blog_link.rb
@@ -2,6 +2,7 @@ class BlogLink < Booqcms::Entry
   content_attr :url, :string
   content_attr :quote, :text
   content_attr :comment, :text
+  content_attr :body, :text
 
   validates :url, presence: true
   validates :quote, presence: true
