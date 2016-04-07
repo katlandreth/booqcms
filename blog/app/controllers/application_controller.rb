59e5389497444c6db1593f5889de7b5aa9da3322
Kat Landreth <katlandreth@gmail.com>
2016-04-07 13:22:44 -0500

    install and configure devise, rolify, and cancan

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper Booqcms::MarkdownHelper
 
+  def after_sign_in_path_for(resource)
+    booqcms.entries_path
+  end
+
 end
--- blog/config/initializers/booqcms.rb
+++ blog/config/initializers/booqcms.rb
@@ -1,3 +1,11 @@
 Booqcms.configure do |config|
   config.content_classes = %w(BlogPost Marketing)
+
+  config.current_user_lookup do
+    env['warden'].user
+  end
+
+  config.sign_in_url do
+    Rails.application.routes.url_helpers.new_user_session_path
+  end
 end
