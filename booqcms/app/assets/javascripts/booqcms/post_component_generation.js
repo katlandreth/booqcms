e80298fe4db7c3614dd85dbb38b7c6b97d88d865
Kat Landreth <katlandreth@gmail.com>
2016-04-08 12:00:51 -0500

    replaces video icon with youtube thumb, displays drag instructions on submit button, resets drag message on drop

--- booqcms/app/assets/javascripts/booqcms/post_component_generation.js
+++ booqcms/app/assets/javascripts/booqcms/post_component_generation.js
@@ -1,28 +1,42 @@
 $(document).ready(function() {
   var video_embed_markup;
+  var video_thumbnail;
+
+  $(".video_embed_form .submit").on("click", function() {
+    create_video_markup();
+    get_video_thumbnail();
+    $(this).addClass("component-created").text("drag the video into place");
+    $(".youtube-component-thumb").replaceWith(video_thumbnail);
+  });
 
   function create_video_markup(){
     var video_id = $("#video_id_field").val();
     video_embed_markup = $.trim('<div class="video">\n \
   <div class="video-wrapper">\n \
-    <iframe src="https://www.youtube.com/embed/'+ video_id +' "frameborder="0" allowfullscreen></iframe>\n \
+    <iframe src="https://www.youtube.com/embed/'+ video_id +
+    ' "frameborder="0" allowfullscreen></iframe>\n \
   </div>\n \
 </div>');
   };
 
-  $(".video_embed_form .submit").on("click", function() {
-    create_video_markup();
-    alert(video_embed_markup);
-  });
+  function get_video_thumbnail() {
+    var video_id = $("#video_id_field").val().trim();
+    video_thumbnail = $.trim('<img src="http://img.youtube.com/vi/'+ video_id +
+    '/mqdefault.jpg" class="youtube-component-thumb"/>');
+  };
 
-var dragComponent = document.getElementById('content-component-video');
+  var dragComponent = document.getElementById('content-component-video');
 
-dragComponent.ondragstart = function(evt) {
+  dragComponent.ondragstart = function(evt) {
     evt.dataTransfer.setData("text/plain", video_embed_markup);
-};
+  };
 
-dragComponent.onclick = function(evt) {
+  dragComponent.onclick = function(evt) {
     evt.preventDefault();
-};
+  };
+
+  $(".textarea-body").on("drop", function(evt) {
+    $(".video_embed_form .submit").removeClass("component-created").text("submit");
+  });
 
 });
