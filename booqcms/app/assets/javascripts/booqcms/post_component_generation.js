5eb79a1769b25d4eaa5e01852ac4c308c3728ab8
Kat Landreth <katlandreth@gmail.com>
2016-04-08 10:18:03 -0500

    grab youtube video id insteafd of URL to generate proper embed markup

--- booqcms/app/assets/javascripts/booqcms/post_component_generation.js
+++ booqcms/app/assets/javascripts/booqcms/post_component_generation.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   var video_embed_markup;
 
   function create_video_markup(){
-    var video_url = $("#video_url_field").val();
+    var video_id = $("#video_id_field").val();
     video_embed_markup = $.trim('<div class="video">\n \
   <div class="video-wrapper">\n \
-    <iframe src="'+ video_url +'"frameborder="0" allowfullscreen></iframe>\n \
+    <iframe src="https://www.youtube.com/embed/'+ video_id +' "frameborder="0" allowfullscreen></iframe>\n \
   </div>\n \
 </div>');
   };
