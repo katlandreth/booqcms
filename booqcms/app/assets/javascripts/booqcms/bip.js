153b1375f2fc04c422bfb1a896f3c928f52380ea
Kat Landreth <katlandreth@gmail.com>
2016-05-09 09:57:09 -0500

    remove unused JS files

--- booqcms/app/assets/javascripts/booqcms/bip.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function() {
-//   jQuery(".best_in_place").best_in_place();
-//
-//   $.datepicker.setDefaults({
-//     showOn: "both",
-//     buttonImageOnly: false,
-//     buttonText: "Calendar"
-//   });
-// });
--- booqcms/app/assets/javascripts/booqcms/publish_status.js
+++
@@ -1,5 +0,0 @@
-// $('form#edit_entry input[type=submit]').click(function() {
-//   $('.bip').each(function(){
-//     $(this).attr('disabled', 'true');
-//   });
-// });
