90125eb6813592e8751a8b763598bbf5ced897ac
Kat Landreth <katlandreth@gmail.com>
2016-05-24 09:22:00 -0500

    remove faulty dummy app migrations

--- booqcms/test/dummy/db/migrate/20160426161102_change_payload_data.rb
+++
@@ -1,9 +0,0 @@
-class ChangePayloadData < ActiveRecord::Migration
-  def self.up
-    change_column :booqcms_entries, :payload, :text
-  end
-
-  def self.down
-    change_column :booqcms_entries, :payload, :json
-  end
-end
