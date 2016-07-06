ba1aa9bd0bb59326c623e330fa5b3ecd72cf5659
Kat Landreth <katlandreth@gmail.com>
2016-07-06 16:25:25 -0500

    add clear highlighting button to seo tool

--- dashboard/app/assets/javascripts/dashboard/highlight_seo.js
+++ dashboard/app/assets/javascripts/dashboard/highlight_seo.js
@@ -1,9 +1,11 @@
 $(document).on('click', '.seo-word', function(){
   var word = $(this).html();
-
   function onInputRegex(input) {
     return new RegExp(word, 'gi');
   }
-
   $('textarea#entry_payload').highlightWithinTextarea(onInputRegex);
+});
+
+$(document).on('click', '.clear-highlighting', function(){
+  $('textarea#entry_payload').data('hwt').destroy();
 });
