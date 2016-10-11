1fa7c55c01989af898c89e03e6d40dcaa216b7ae
Kat Landreth <katlandreth@gmail.com>
2016-10-11 15:06:25 -0500

    makes tags on post carts plain text instead of links, styles the card to highlight the tags, adds privacy policy link to book sales page, edits some of the policy copy

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -2,7 +2,7 @@ module TagHelper
 
   def show_tags(post)
     if post.all_tags.length != 0
-      tags = "tags:" + " " + tag_links(post.all_tags)
+      tags = "tags:" + " " + post.all_tags
       tags.html_safe
     else
       return
