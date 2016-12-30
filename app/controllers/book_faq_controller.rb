2d0e1dde3a2d1b2d3ff1c252c145fd86b25aa7da
Kat Landreth <katlandreth@gmail.com>
2016-12-30 16:23:02 -0500

    updates faq

--- app/controllers/book_faq_controller.rb
+++ app/controllers/book_faq_controller.rb
@@ -1,4 +1,5 @@
 class BookFaqController < ApplicationController
   def show
+    @post = Post.new(title: "FAQ", featured_image: "#{view_context.image_url('blackandwhite_desk_image.jpg')}")
   end
 end
